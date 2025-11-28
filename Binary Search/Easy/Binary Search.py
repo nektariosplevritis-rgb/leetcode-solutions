1bfb531d5b1a399735b79f8085413db9ef38e93c
nektariosplevritis-rgb <nektarios.plevritis@gmail.com>
2025-11-28 03:59:24 +0200

    Update and rename easy/Binary Search.py to Binary Search/Easy/Binary Search.py

---
+++ Binary Search/Easy/Binary Search.py
@@ -0,0 +1,15 @@
+# 704. Binary Search
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+                
+        return -1
